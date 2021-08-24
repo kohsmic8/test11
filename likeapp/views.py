3966bb65622ad95c343379e80e11e0e80151c1bb
rhgud <kohsmic8@gmail.com>
2021-08-24 12:30:28 +0900

    8.24 test11 commit4

--- likeapp/views.py
+++ likeapp/views.py
@@ -12,19 +12,21 @@ from likeapp.models import LikeRecord
 
 @method_decorator(login_required, 'get')
 class LikeArticleView(RedirectView):
-
     def get(self, request, *args, **kwargs):
         user = request.user
         article = Article.objects.get(pk=kwargs['article_pk'])
 
-        like_record = LikeRecord.objects.filter(user=user, article=article)
-
+        like_record = LikeRecord.objects.filter(user=user,
+                                                article=article)
         if like_record.exists():
-            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk': kwargs['article_pk']}))
+            return HttpResponseRedirect(reverse('articleapp:detail',
+                                                kwargs={'pk': kwargs['article_pk']}))
         else:
             LikeRecord(user=user, article=article).save()
 
         article.like += 1
         article.save()
-
         return super().get(request, *args, **kwargs)
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('articleapp:detail', kwargs={'pk': kwargs['article_pk']})
