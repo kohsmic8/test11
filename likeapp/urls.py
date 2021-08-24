1cce25d3e21d0f571bac7aaa563f89daf4b4a2ee
rhgud <kohsmic8@gmail.com>
2021-08-24 12:03:30 +0900

    8.24 test11 commit3

--- likeapp/urls.py
+++ likeapp/urls.py
@@ -1,5 +1,7 @@
+from django.urls import path
 
+from likeapp.views import LikeArticleView
 
 urlpatterns = [
-
+    path('article/<int:article_pk>', LikeArticleView.as_view(), name='article_like'),
 ]
